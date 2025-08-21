8611f73dbc9ec95734656284bd185d848ebf460c
Alan <143666779+Alanmilo30@users.noreply.github.com>
2025-08-20 19:00:14 -0600

    Cambio 2

--- Hola mundo.py
+++ Hola mundo.py
@@ -1,9 +1,14 @@
 from datetime import datetime
+import random
 
 nombre = input("¿Cómo te llamas? ")
 momento = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+numero_suerte = random.randint(1, 100)  # Número aleatorio entre 1 y 100
 
-print(f"Hola, {nombre}! Bienvenido al mundo de Python.")
+print(f"\nHola, {nombre}! Bienvenido al mundo de Python.")
 print(f"Fecha y hora actuales: {momento}")
+print(f"Tu nombre tiene {len(nombre)} caracteres.")
+print(f"Tu número de la suerte para hoy es: {numero_suerte}")
+
 
 
